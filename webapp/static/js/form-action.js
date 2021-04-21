83053cc43525068644c73a8e5d12bd0c554f4230
Nithin Chintala <chintala.v@northeastern.edu>
2021-04-21 18:40:20 -0400

    new css stuff

--- webapp/static/js/form-action.js
+++ webapp/static/js/form-action.js
@@ -3,3 +3,17 @@ function setForm(method, action) {
     document.getElementById("form").method = method
     document.getElementById("form").submit()
 }
+
+(function(window, document, undefined){
+    window.onload = init;
+    function init(){
+        searchBar =  document.getElementById("search")
+        if (searchBar != null) {
+            searchBar.addEventListener("keydown", function(event) {
+            if (event.keyCode == 13) {
+                location.href = "/papers?tag=" + searchBar.value
+            }
+            })
+        }
+    }
+})(window, document, undefined);
